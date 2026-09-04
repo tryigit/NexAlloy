7e471a5931d51cb1e466b0e767af232b49e1eb2c
Yiğit <40565628+tryigit@users.noreply.github.com>
2026-09-04 19:37:05 +0300

    Reduce player response hook allocations

--- app/src/main/java/io/github/nexalloy/morphe/youtube/video/playerresponse/PlayerResponseMethodHookPatch.kt
+++ app/src/main/java/io/github/nexalloy/morphe/youtube/video/playerresponse/PlayerResponseMethodHookPatch.kt
@@ -2,34 +2,39 @@ package io.github.nexalloy.morphe.youtube.video.playerresponse
 
 import io.github.nexalloy.patch
 
-private val hooks = mutableSetOf<Hook<*>>()
+private val beforeVideoIdHooks = linkedSetOf<Hook.ProtoBufferParameterBeforeVideoId>()
+private val videoIdHooks = linkedSetOf<Hook.VideoId>()
+private val playlistIdHooks = linkedSetOf<Hook.PlaylistId>()
+private val afterVideoIdHooks = linkedSetOf<Hook.ProtoBufferParameter>()
 
 fun addPlayerResponseMethodHook(hook: Hook<*>) {
-    hooks += hook
+    when (hook) {
+        is Hook.ProtoBufferParameterBeforeVideoId -> beforeVideoIdHooks += hook
+        is Hook.VideoId -> videoIdHooks += hook
+        is Hook.PlaylistId -> playlistIdHooks += hook
+        is Hook.ProtoBufferParameter -> afterVideoIdHooks += hook
+    }
 }
 
 val PlayerResponseMethodHook = patch {
     val PARAMETER_VIDEO_ID = 0
     val PARAMETER_PROTO_BUFFER = 2
     val PARAMETER_PLAYLIST_ID = 3
-    var parameterIsShortAndOpeningOrPlaying = -1
-    ::playerParameterBuilderFingerprint.dexMethod.apply {
-        parameterIsShortAndOpeningOrPlaying =
-            paramTypeNames.zip(paramTypeNames.indices)
-                .indexOfFirst { (type, i) -> i >= 10 && type == "boolean" }
-    }.hookMethod {
+    val parameterIsShortAndOpeningOrPlaying =
+        ::playerParameterBuilderFingerprint.dexMethod.paramTypeNames
+            .indexOfFirst { type -> type == "boolean" && ::playerParameterBuilderFingerprint.dexMethod.paramTypeNames.indexOf(type) >= 10 }
+
+    require(parameterIsShortAndOpeningOrPlaying >= 0) {
+        "Player parameter builder is missing the Shorts state boolean parameter"
+    }
+
+    ::playerParameterBuilderFingerprint.hookMethod {
         before { param ->
             val videoId = param.args[PARAMETER_VIDEO_ID] as String
             var protobuf = param.args[PARAMETER_PROTO_BUFFER] as String
             val playlistId = param.args[PARAMETER_PLAYLIST_ID] as String?
             val isShortAndOpeningOrPlaying =
                 param.args[parameterIsShortAndOpeningOrPlaying] as Boolean
-
-            val beforeVideoIdHooks =
-                hooks.filterIsInstance<Hook.ProtoBufferParameterBeforeVideoId>()
-            val videoIdHooks = hooks.filterIsInstance<Hook.VideoId>()
-            val playlistIdHooks = hooks.filterIsInstance<Hook.PlaylistId>()
-            val afterVideoIdHooks = hooks.filterIsInstance<Hook.ProtoBufferParameter>()
 
             beforeVideoIdHooks.forEach {
                 protobuf = it(protobuf, videoId, isShortAndOpeningOrPlaying)
