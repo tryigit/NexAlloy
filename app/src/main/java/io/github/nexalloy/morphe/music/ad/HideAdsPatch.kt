8f38c594158c04ea1da1b566cffa555257d98bdf
Yiğit <40565628+tryigit@users.noreply.github.com>
2026-09-04 14:44:43 +0300

    fix(music): hide premium view at target call

--- app/src/main/java/io/github/nexalloy/morphe/music/ad/HideAdsPatch.kt
+++ app/src/main/java/io/github/nexalloy/morphe/music/ad/HideAdsPatch.kt
@@ -2,7 +2,6 @@ package io.github.nexalloy.morphe.music.ad
 
 import android.view.View
 import app.morphe.extension.music.patches.HideAdsPatch
-import app.morphe.extension.shared.Logger
 import app.morphe.extension.shared.ResourceUtils
 import io.github.nexalloy.morphe.music.misc.settings.PreferenceScreen
 import io.github.nexalloy.morphe.shared.ad.HideFullscreenAds
@@ -24,21 +23,13 @@ val HideAds = patch(
         SwitchPreference("morphe_music_hide_video_ads"),
     )
 
-    // Hide 'Get Music Premium' label
-    HideGetPremiumFingerprint.hookMethod {
-        val id = ResourceUtils.getIdIdentifier("unlimited_panel")
-        after { param ->
-            val thiz = param.thisObject
-            for (field in thiz.javaClass.fields) {
-                val view = field.get(thiz)
-                if (view !is View) continue
-                val panelView = view.findViewById<View>(id) ?: continue
-                Logger.printDebug { "hide get premium" }
-                panelView.visibility = View.GONE
-                break
+    HideGetPremiumFingerprint.hookMethod(
+        scopedHook(::hideGetPremiumSetVisibility.member) {
+            before { param ->
+                param.args[0] = View.GONE
             }
         }
-    }
+    )
 
     MembershipSettingsFingerprint.hookMethod {
         before {
