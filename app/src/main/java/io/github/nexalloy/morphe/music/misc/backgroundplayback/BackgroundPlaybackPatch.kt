c1ea7fc468e3ce44aee01e80556c54cf44eb6665
Yiğit <40565628+tryigit@users.noreply.github.com>
2026-09-04 15:22:06 +0300

    Fix Music background playback dependency

--- app/src/main/java/io/github/nexalloy/morphe/music/misc/backgroundplayback/BackgroundPlaybackPatch.kt
+++ app/src/main/java/io/github/nexalloy/morphe/music/misc/backgroundplayback/BackgroundPlaybackPatch.kt
@@ -1,12 +1,15 @@
 package io.github.nexalloy.morphe.music.misc.backgroundplayback
 
 import de.robv.android.xposed.XC_MethodReplacement
+import io.github.nexalloy.morphe.shared.misc.CheckRecycleBitmapMediaSession
 import io.github.nexalloy.patch
 
 val BackgroundPlayback = patch(
     name = "Remove background playback restrictions",
     description = "Removes restrictions on background playback, including playing kids videos in the background.",
 ) {
+    dependsOn(CheckRecycleBitmapMediaSession)
+
     BackgroundPlaybackDisableFingerprint.hookMethod(XC_MethodReplacement.returnConstant(true))
     KidsBackgroundPlaybackPolicyControllerFingerprint.hookMethod(XC_MethodReplacement.DO_NOTHING)
 }
