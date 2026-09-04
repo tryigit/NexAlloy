d2b3156c06e0476b1a32208eabdc327f5dcf56c5
Yiğit <40565628+tryigit@users.noreply.github.com>
2026-09-04 10:09:48 +0300

    Handle Cronet failures without response info

--- app/src/main/java/io/github/nexalloy/morphe/youtube/misc/imageurlhook/CronetImageUrlHook.kt
+++ app/src/main/java/io/github/nexalloy/morphe/youtube/misc/imageurlhook/CronetImageUrlHook.kt
@@ -10,7 +10,6 @@ import java.lang.reflect.Field
 val cronetImageUrlHookPatch = patch(
     description = "Hooks Cronet image urls.",
 ) {
-    // loadImageUrlMethod
     MessageDigestImageUrlFingerprint.hookMethod {
         before { param ->
             var url = param.args[0] as String? ?: return@before
@@ -20,7 +19,7 @@ val cronetImageUrlHookPatch = patch(
             param.args[0] = url
         }
     }
-    // loadImageSuccessCallbackMethod
+
     OnSucceededFingerprint.hookMethod {
         before { param ->
             imageUrlSuccessCallbackHook.forEach {
@@ -29,21 +28,17 @@ val cronetImageUrlHookPatch = patch(
         }
     }
 
-    // loadImageErrorCallbackMethod
     OnFailureFingerprint.hookMethod {
         before { param ->
+            val request = param.args[0] as UrlRequest
+            val responseInfo = param.args[1] as? UrlResponseInfo
+            val exception = param.args[2] as IOException
             imageUrlErrorCallbackHook.forEach {
-                it(
-                    param.args[0] as UrlRequest,
-                    param.args[1] as? UrlResponseInfo ?: return@before,
-                    param.args[2] as IOException
-                )
+                it(request, responseInfo, exception)
             }
         }
     }
 
-    // The URL is required for the failure callback hook, but the URL field is obfuscated.
-    // Add a helper get method that returns the URL field.
     urlJField = ::urlField.field
 }
 
@@ -53,26 +48,16 @@ fun getHookedUrl(o: CronetUrlRequest) = urlJField.get(o) as String
 
 private var imageUrlHooks = listOf<(String) -> String>()
 private var imageUrlSuccessCallbackHook = listOf<(UrlRequest, UrlResponseInfo) -> Unit>()
-private var imageUrlErrorCallbackHook = listOf<(UrlRequest, UrlResponseInfo, IOException) -> Unit>()
+private var imageUrlErrorCallbackHook = listOf<(UrlRequest, UrlResponseInfo?, IOException) -> Unit>()
 
-/**
- * @param highPriority If the hook should be called before all other hooks.
- */
 fun addImageUrlHook(f: (String) -> String, highPriority: Boolean = false) {
     imageUrlHooks = if (highPriority) listOf(f) + imageUrlHooks else imageUrlHooks + listOf(f)
 }
 
-/**
- * If a connection completed, which includes normal 200 responses but also includes
- * status 404 and other error like http responses.
- */
 fun addImageUrlSuccessCallbackHook(f: (UrlRequest, UrlResponseInfo) -> Unit) {
     imageUrlSuccessCallbackHook += listOf(f)
 }
 
-/**
- * If a connection outright failed to complete any connection.
- */
-fun addImageUrlErrorCallbackHook(f: (UrlRequest, UrlResponseInfo, IOException) -> Unit) {
+fun addImageUrlErrorCallbackHook(f: (UrlRequest, UrlResponseInfo?, IOException) -> Unit) {
     imageUrlErrorCallbackHook += listOf(f)
 }
